09de59be1f000ef8dfdd4ecb3b75d80880e691c8
ying.shuang@cn.abb.com <ying.shuang@cn.abb.com>
2021-08-06 15:57:55 +0800

    added compression logic

---
+++ Utilities/Compression/Compression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Utilities
+{
+    public class compression
+    {
+        public byte[] compression(string compressionType)
+        {
+            if (compressionType.Equals(CompressionType.Deflate))
+            {
+                using var compressedStream = new MemoryStream();
+                using (var deflateStream = new DeflateStream(compressedStream, CompressionMode.Compress))
+                    deflateStream.Write(bytes, 0, bytes.Length);
+
+                return compressedStream.ToArray();
+            }
+
+            if (compressionType.Equals(CompressionType.GZip))
+            {
+                using var compressedStream = new MemoryStream();
+                using (var gZipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+                    gZipStream.Write(bytes, 0, bytes.Length);
+
+                return compressedStream.ToArray();
+            }
+        }
+    }
+}
